1fe11855421494eafa42f596b82eb097eefee767
Erik De Roo <ede-roo@f1r4s3.codam.nl>
2026-09-06 16:50:26 +0200

    3rd commit met structs

--- 1Raycast/header_raycast.h
+++ 1Raycast/header_raycast.h
@@ -2,5 +2,36 @@
 # define HEADER_RAYCAST_H
 #include "../header.h"
 
+typedef struct s_img
+{
+	void	*ptr;       /* from mlx_new_image / mlx_xpm_file_to_image */
+	char	*addr;      /* from mlx_get_data_addr */
+	int		bpp;
+	int		line_len;
+	int		endian;
+	int		w;
+	int		h;
+}	t_img;
+
+typedef struct s_player
+{
+	double	x;          /* position, in tile units */
+	double	y;
+	double	dir_x;      /* direction vector, length 1 */
+	double	dir_y;
+	double	plane_x;    /* camera plane, perpendicular to dir */
+	double	plane_y;
+	int		keys[6];    /* W A S D LEFT RIGHT held? */
+}	t_player;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	t_img		screen;
+	t_img		tex[4];     /* NO SO WE EA, loaded from cfg->tex_path */
+	t_config	*cfg;       /* map + colours live here, don't copy them */
+	t_player	player;
+}	t_game;
 
 #endif
--- header.h
+++ header.h
@@ -1,6 +1,17 @@
 #ifndef HEADER_H
 # define HEADER_H
 
-
+/*It's just "what the file said": 4 texture paths, 2 colours, the map, the start tile and letter. Parser fills it, raycaster reads it. Nothing about mlx in here.*/
+typedef struct s_config
+{
+	char	*tex_path[4];   /* NO, SO, WE, EA */
+	int		floor_rgb;      /* packed 0xRRGGBB */
+	int		ceil_rgb;
+	char	**map;          /* jagged, NULL-terminated */
+	int		map_h;
+	double	player_x;       /* center of start tile, e.g. 3.5 */
+	double	player_y;
+	char	player_dir;     /* 'N','S','E','W' */
+}	t_config;
 
 #endif
