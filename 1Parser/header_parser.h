5942fcbebb4913597e459a952b619de9899412a3
Simcha De Lange <side-lan@f1r4s5.codam.nl>
2026-09-06 17:41:47 +0200

    struct inits, file kopie functie en main header includes toegevoegd

--- 1Parser/header_parser.h
+++ 1Parser/header_parser.h
@@ -3,4 +3,24 @@
 #include "../header.h"
 
 
+typedef struct s_parser
+{
+	int 	fd;
+	char	*text;
+	char	**map_copy;
+	char	**map;
+	
+}				t_parser;
+
+
+//init structs
+void	init_main_struct(t_config *data);
+void	init_parse_struct(t_parser *p_data);
+
+//main
+int		parse_main(char *argv[]);
+
+
+
+
 #endif
